576f8206bb35af9d0228c528e88bf08cc09f470c
Harry <hjespley@gmail.com>
2023-10-17 19:52:13 +0100

    hiya

--- base.py
+++ base.py
@@ -4,7 +4,8 @@ from dataclasses import dataclass
 class Player: 
     player_id: int 
     name: str 
-    composite_score: float 
+    composite_score: float  
+    composite_score3: float 
     cost: float 
     team: int 
     position: int 
@@ -30,13 +31,30 @@ class DBHandler:
     pass 
 
 class Optimiser: 
+    lineup_size = 11 
+    bench_size = 4 
+    team_max = 3 
+    formation_constraints = { 
+        1: (1, 1), 
+        2: (3, 5), 
+        3: (2, 5), 
+        4: (1, 3) 
+    } 
+    position_constraints = { 
+        1: 2, 
+        2: 5, 
+        3: 5,
+        4: 3
+    }
+
+class TransferOptimiser(Optimiser): 
     pass 
 
-class TransferOptimiser: 
-    pass 
-
-class ChipOptimiser: 
-    pass 
+class ChipOptimiser(Optimiser): 
+    chip_switcher = { 
+        'free_hit': lambda player: player.composite_score, 
+        'wildcard': lambda player: player.composite_score3 
+    }
 
 def player_rating(player): 
     pass 
