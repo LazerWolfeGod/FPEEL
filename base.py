8cd3287562a4940929f7e6264b106dee74bb952d
Harry <hjespley@gmail.com>
2023-10-17 20:06:35 +0100

    updated_base

--- base.py
+++ base.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass 
+import pulp 
 
 @dataclass 
 class Player: 
@@ -54,7 +55,77 @@ class ChipOptimiser(Optimiser):
     chip_switcher = { 
         'free_hit': lambda player: player.composite_score, 
         'wildcard': lambda player: player.composite_score3 
-    }
+    } 
+    @classmethod 
+    def captain_chooser(cls, team: list) -> dict: 
+        return { 
+            'captain': max(team, key=lambda player: player.composite_score), 
+            'vice_captain': max(team, key=lambda player: player.composite_score3) 
+        } 
+    
+    @classmethod 
+    def bench_boost_predict(cls, team: list) -> float: 
+        return sum([player.composite_score for player in team[11:]]) 
+    
+    def interpolate_budget(cls, bench_importance: float, budget: float) -> float: 
+        if bench_importance < 0 or bench_importance > 1: 
+            raise ValueError('Bench importance value must be between 0 and 1') 
+        min_multi = 0.83 
+        max_multi = (220/3)/100 
+        return budget*(min_multi + (max_multi - min_multi)*bench_importance)  
+
+    def get_position_requirements(cls, team: list): 
+        return {
+            1: cls.position_constraints[1] - sum(1 for player in team if player.position == 1), 
+            2: cls.position_constraints[2] - sum(1 for player in team if player.position == 2), 
+            3: cls.position_constraints[3] - sum(1 for player in team if player.position == 3), 
+            4: cls.position_constraints[4] - sum(1 for player in team if player.position == 4) 
+        } 
+    
+    def generate_starters(cls, players: list, budget: float, chip_type: str):  
+        key_function = cls.chip_switcher[chip_type] 
+        problem = pulp.LpProblem('Starting_11_Optimiser', pulp.LpMaximize) 
+        selected = pulp.LpVariable.dicts('Starters', [player.player_id for player in players], cat=pulp.LpBinary) 
+
+        problem += pulp.lpSum(key_function(player)*selected[player.player_id] for player in players)   
+        problem += pulp.lpSum(selected[player.player_id] for player in players) == cls.lineup_size 
+        problem += pulp.lpSum(player.cost*selected[player.player_id] for player in players) <= budget 
+        for position, (min, max) in cls.formation_constraints.items(): 
+            problem += pulp.lpSum(selected[player.player_id] for player in players if player.position == position) >= min 
+            problem += pulp.lpSum(selected[player.player_id] for player in players if player.position == position) <= max 
+        for team in range(1, 21): 
+            problem += pulp.lpSum(selected[player.player_id] for player in players if player.team == team) <= cls.team_max 
+        
+        problem.solve() 
+        selected_team = [player for player in players if pulp.value(selected[player.player_id])==1]  
+        budget_used = sum([player.cost for player in selected_team]) 
+        return sorted(selected_team, key=lambda x: x.position), budget_used  
+    
+    
+    def generate_bench(cls, players: list, budget: float, positions_left: dict, selected_11: list, chip_type: str) -> list:  
+        key_function = cls.chip_switcher[chip_type]  
+        problem = pulp.LpProblem('FPL_Optimiser', pulp.LpMaximize) 
+        selected = pulp.LpVariable.dicts('Bench', [player.player_id for player in players], cat=pulp.LpBinary) 
+
+        problem += pulp.lpSum(key_function(player)*selected[player.player_id] for player in players) 
+        problem += pulp.lpSum(selected[player.player_id] for player in players) == cls.bench_size 
+        problem += pulp.lpSum(player.cost*selected[player.player_id] for player in players) <= budget  
+        problem += pulp.lpSum(selected[player] for player in players if player not in selected_11) 
+        for position, value in positions_left.items(): 
+            problem += pulp.lpSum(selected[player.player_id] for player in players if player.position == position) == value 
+        problem.solve() 
+        selected_bench = [player for player in players if pulp.value(selected[player.player_id])==1]  
+        return sorted(selected_bench, key=lambda x: x.position) 
+    
+    @classmethod 
+    def generate_team(self, players: list, budget: float, bench_importance: float, chip_type: str) -> list: 
+        starter_budget = self.interpolate_budget(self, bench_importance, budget) 
+        result_11, budget_used = self.generate_starters(self, players, starter_budget, chip_type)  
+        bench_budget = budget-budget_used  
+        positions_left = self.get_position_requirements(self, result_11) 
+        result_bench = self.generate_bench(self, players, bench_budget, positions_left, players, chip_type)
+        return result_11+result_bench
+
 
 def player_rating(player): 
     pass 
