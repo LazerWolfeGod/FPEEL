fc3a6d4fb5f0e28df3227155ec5a8d222fa97785
Harry Espley <harry.espley@gw.legal>
2023-10-19 20:16:03 +0100

    message

--- ui2.py
+++ ui2.py
@@ -14,7 +14,65 @@ import utils
 import base  
 
 class WindowParent(QMainWindow): 
-    pass 
+    def __init__(self, previous_window=None, fpl=None): 
+        super().__init__() 
+        self.previous_window = previous_window 
+        self.fpl = fpl  
+        self.window_id = None 
+        self.window_switcher = { 
+            0: LoginWindow, 
+            1: MainWindow, 
+            2: LineupWindow, 
+            3: LeagueWindow,  
+            4: DatabaseWindow, 
+            5: SettingsWindow, 
+            6: ExitWindow
+        } 
+        self.colour_switcher = { 
+            0: f'background-color: {self.settings.colour_scheme.primary_colour}'f'color: {self.settings.colour_scheme.secondary_colour}', 
+            1: f'background-color: {self.settings.colour_scheme.secondary_colour}'f'color: {self.settings.colour_scheme.primary_colour}' 
+        } 
+    
+    def refresh(self): 
+        self.close() 
+        self.new_window = self.window_switcher[self.window_id](fpl=self.fpl, previous_window=self.previous_window) 
+
+    def open_window(self, window): 
+        if window != 6: 
+            self.close() 
+        self.new_window = self.window_switcher[self.window_id](fpl=self.fpl, previous_window=self.window_id) 
+
+class Settings: 
+    def __init__(self): 
+        self.settings_path = os.path.join(os.getcwd(), 'json_data', 'settings.json') 
+        try: 
+            settings = utils.read_jsoN(self.settings_path) 
+        except json.JSONDecodeError: 
+            settings = { 
+                'primary_colour': None, 
+                'secondary_colour': None, 
+                'font': 'Arial', 
+                'button_sound': None, 
+                'button_volume': None 
+            } 
+            utils.write_json(settings, self.settings_path) 
+        self.colour_scheme = ColourScheme(settings['primary_colour'], settings['secondary_colour']) 
+        self.font = settings['font'] 
+        self.button_sound = settings['button_sound'] 
+        self.button_volume = settings['button_volume'] 
+    
+    def change_settings(self, settings_dict): 
+        settings = utils.read_json(self.settings_path)
+        for k in settings_dict: 
+            settings[k] = settings_dict[k] 
+            self.settings_switcher[k] = settings_dict[k] 
+        utils.write_json(self.settings_path, settings) 
+
+@dataclass 
+class ColourScheme: 
+    primary_colour: str = None 
+    secondary_colour: str = None 
+    error_colour = 'rgb(255, 0, 0);' 
 
 class LoginWindow(WindowParent): 
     pass  
@@ -43,3 +101,9 @@ class ExitWindow(WindowParent):
 
 
 
+if __name__ == '__main__': 
+    app = QtWidgets.QApplication(sys.argv) 
+    window = WindowParent()
+    window.show()  
+    print(window.previous_window) 
+    sys.exit(app.exec()) 
